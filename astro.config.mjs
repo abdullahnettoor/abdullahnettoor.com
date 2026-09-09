f0c9032d6ccc816422ee4316f0eaaffe35e4efdc
Abdullah Nettoor <abdullahnettoor@gmail.com>
2026-09-10 05:27:46 +0530

    fix: align generated routes with Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://abdullahnettoor.com",
   output: "static",
-  trailingSlash: "never",
+  trailingSlash: "always",
   integrations: [sitemap()],
   build: {
     format: "directory"
